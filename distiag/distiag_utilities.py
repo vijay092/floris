ef6a942a511354f0b256bb6b09542f56f543924d
Paul <pstanfel@mymail.mines.edu>
2019-11-26 14:21:17 -0700

    Added tile coding method.

--- distiag/distiag_utilities.py
+++ distiag/distiag_utilities.py
@@ -3,16 +3,20 @@ import floris.tools as wfct
 
 import numpy as np
 
-# Agent object that encapsulates necessary turbine information
 class Agent():
+    """
+    Agent object that encapsulates necessary turbine information 
+    """
     def __init__(self, turbine, alias, coordinates):
         self.turbine = turbine
         self.alias = alias
         self.coordinates = coordinates
         return
 
-# creates a list of agents that will be used by the environment to calculate reward
 def initialize_agent_list(fi, layout_array):
+    """
+    Creates a list of agents that will be used by the environment to calculate reward
+    """
     agents = []
     layout_x = layout_array[0]
     layout_y = layout_array[1]
@@ -41,3 +45,28 @@ def create_grid(lower_bounds, upper_bounds, bins, offsets):
         grid.append(row)
 
     return np.array(grid)
+
+def create_tiling(lower_bounds, upper_bounds, bins_lists, offsets_lists):
+    """
+    Create an array of grids that can be used for tile coding. bins_lists is an array of iterables that
+    specify the bin numbers for each of the dimensions of each grid. offsets_lists is an array of 
+    iterables that specify the offsets for each of the dimensions of each grid.
+
+    Returns a numpy array of grids
+    """
+    if len(lower_bounds) != len(upper_bounds) or len(bins_lists) != len(offsets_lists) or len(bins_lists) != len(lower_bounds):
+        raise ValueError('Tiling dimensions do not agree')
+
+    tiling = []
+
+    for lower_bound,upper_bound,bins,offsets in zip(lower_bounds, upper_bounds, bins_lists, offsets_lists):
+        grid = create_grid(lower_bound, upper_bound, bins, offsets)
+        tiling.append(grid)
+
+    return np.array(tiling)
+
+    def encode_state(tiling, state):
+        """
+        Takes a multi-dimensional state observation and enocdes it using a given tiling.
+        """
+        return
