e9d18b3645063d0f3574f4dce0c3d387b448c0a2
Paul <pstanfel@mymail.mines.edu>
2019-11-26 11:17:29 -0700

    Added calculate_wake() method so that power can be calculated.

--- distiag/env-test.py
+++ distiag/env-test.py
@@ -14,7 +14,7 @@ layout_y = [0, 0, 0]
 
 # create FlorisUtilities object with the correct layout of turbines
 fi.reinitialize_flow_field(layout_array=[layout_x, layout_y])
-
+fi.calculate_wake()
 # create a list of agents that is assembled using the FlorisUtilities object
 agents = du.initialize_agent_list(fi, [layout_x, layout_y])
 
