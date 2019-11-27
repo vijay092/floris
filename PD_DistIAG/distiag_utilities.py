d62c48eec07675aab620404b1bd9e01111997964
Paul <pstanfel@mymail.mines.edu>
2019-11-27 15:24:43 -0700

    Created PD-DistIAG skeleton code to be filled in.

---
+++ PD_DistIAG/distiag_utilities.py
@@ -0,0 +1,7 @@
+def run_pd_distiag(training_data, initial_theta, initial_w, rho, M, sigma_theta, sigma_w):
+
+    states_tr = training_data[0]
+    next_states_tr = training_data[1]
+    rewards_tr = training_data[2]
+
+    return
