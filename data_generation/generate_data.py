c683ab0e83e0c95a51a71e8f09abe11fcb9e7ee4
Paul <pstanfel@mymail.mines.edu>
2019-11-27 11:29:54 -0700

    Created a simulation that outputs wind farm power to observe trends.

--- data_generation/generate_data.py
+++ data_generation/generate_data.py
@@ -3,10 +3,11 @@ import marl_env
 
 import floris
 import floris.tools as wfct
-
 import utilities as utils
 import sarsa
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 fi = wfct.floris_utilities.FlorisInterface("example_input.json")
 
@@ -23,30 +24,26 @@ agents = utils.initialize_agent_list(fi, [layout_x, layout_y])
 # lower and upper yaw limits
 limits = [-30, 29]
 
+iterations = 1000
+
 # create an environment
-env = gym.make('marl_env:marl-farm-v0', fi=fi, agents=agents, limits=limits)
+env = gym.make('marl_env:marl-farm-v0', fi=fi, agents=agents, limits=limits, iterations=iterations)
 
 lower_bounds = [-30, -30, -30]
 upper_bounds = [30, 30, 30]
-bins = (100, 100, 100)
+bins = (50, 50, 50)
 bins_list = [bins, bins, bins]
 offsets_list = [(-5, -5, -5), (0,0,0), (5, 5, 5)]
 
-test_tiling = utils.create_tiling(lower_bounds, upper_bounds, bins_list, offsets_list)
+tiling = utils.create_tiling(lower_bounds, upper_bounds, bins_list, offsets_list)
 
-test_state = (-25, -25, -25)
+params = {'method':'epsilon', 
+          'epsilon':0.1,
+          'alpha':0.1,
+          'gamma':0.5}
 
-test_encoding = utils.encode_state(test_tiling, test_state)
+outputs = sarsa.generate_trajectory(env, params, tiling)
 
-q_table = sarsa.create_q_table(env)
+plt.plot(outputs[-1])
 
-state = env.reset()
-
-action = sarsa.select_action(q_table[state], 'epsilon', epsilon=0.8)
-
-outputs = env.step(action)
-print("Initial state:", state)
-print("Action:", action)
-print("Next state:", outputs[0])
-
-print(np.shape(sarsa.create_q_table(env)))
+plt.show()
