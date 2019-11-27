cc8864ed206dbb4822c93236728b3b9d8a73c572
Paul <pstanfel@mymail.mines.edu>
2019-11-27 14:48:01 -0700

    Added code to generate trajectory arrays and agent dictionary.

--- data_generation/generate_data.py
+++ data_generation/generate_data.py
@@ -8,6 +8,8 @@ import sarsa
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import pickle
 
 fi = wfct.floris_utilities.FlorisInterface("example_input.json")
 
@@ -24,7 +26,7 @@ agents = utils.initialize_agent_list(fi, [layout_x, layout_y])
 # lower and upper yaw limits
 limits = [-30, 29]
 
-iterations = 1000
+iterations = 100#85000
 
 # create an environment
 env = gym.make('marl_env:marl-farm-v0', fi=fi, agents=agents, limits=limits, iterations=iterations)
@@ -38,12 +40,29 @@ offsets_list = [(-5, -5, -5), (0,0,0), (5, 5, 5)]
 tiling = utils.create_tiling(lower_bounds, upper_bounds, bins_list, offsets_list)
 
 params = {'method':'epsilon', 
-          'epsilon':0.1,
+          'epsilon':0.3,
           'alpha':0.1,
           'gamma':0.5}
 
 outputs = sarsa.generate_trajectory(env, params, tiling)
 
 plt.plot(outputs[-1])
+
+plt.figure()
+for i in range(len(agents)):
+    plt.plot( [state[i] for state in outputs[-2]] )
+
+dir_path = os.path.dirname(os.getcwd())
+
+np.save('states', outputs[0])
+np.save('rewards', outputs[1])
+np.save('next_states', outputs[2])
+
+agent_dict = {agent.alias: agent.coordinates for agent in agents}
+
+with open('agents.pickle', 'wb') as handle:
+    pickle.dump(agent_dict, handle)
+
+np.save('agents', agent_dict, allow_pickle=True)
 
 plt.show()
